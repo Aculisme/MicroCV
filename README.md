af126bfe9aa64e262d1596fde4bed425039ea8ec
Luca Mehl <lsm20@ic.ac.uk>
2021-08-05 00:19:19 +0200

    Filled in README.md, added requirements, and added example images

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MicroCV
+_Petri dish OpenCV blob detection_
+
+## Installation
+
+Assuming python/pip are installed, run 
+`python -m pip install -r requirements.txt` to set up the necessary requirements. replace `python` with your own python installation alias.
+
+## Usage
+
+From the command line, run `python main.py IMAGE_FILENAME`, where `IMAGE_FILENAME` is the petri dish image to analyze (e.g. example1.png).
+
+The provided sliders may be used to refine the feature detection parameters. The sliders correspond to the following parameters:
+
+- min_dist: Minimum distance between detected centers.
+- edge_threshold: Upper threshold for the internal Canny edge detector.
+- centre_threshold: Threshold for center detection.
+- min_radius: Minimum radius to be detected. If unknown, put zero as default.
+- max_radius: Maximum radius to be detected. If unknown, put zero as default.
+
+Quit the python window by pressing `q`, or quitting the process from the command line.
--- main.py
+++ main.py
@@ -11,26 +11,26 @@ def main(argv):
     window_name = 'MicroCV'
     cv2.namedWindow(window_name)
 
-    default_min_centre_distance = 10
+    default_min_dist = 10
     default_edge_threshold = 100
     default_centre_threshold = 25
     default_min_radius = 5
     default_max_radius = 40
 
-    cv2.createTrackbar("min_centre_distance", window_name, default_min_centre_distance, 50, lambda x: x)
+    cv2.createTrackbar("min_dist", window_name, default_min_dist, 50, lambda x: x)
     cv2.createTrackbar("edge_threshold", window_name, default_edge_threshold, 150, lambda x: x)
     cv2.createTrackbar("centre_threshold", window_name, default_centre_threshold, 150, lambda x: x)
     cv2.createTrackbar("min_radius", window_name, default_min_radius, 40, lambda x: x)
     cv2.createTrackbar("max_radius", window_name, default_max_radius, 100, lambda x: x)
 
     while True:
-        min_centre_distance = cv2.getTrackbarPos("min_centre_distance", window_name)
+        min_dist = cv2.getTrackbarPos("min_dist", window_name)
         edge_threshold = cv2.getTrackbarPos("edge_threshold", window_name)
         centre_threshold = cv2.getTrackbarPos("centre_threshold", window_name)
         min_radius = cv2.getTrackbarPos("min_radius", window_name)
         max_radius = cv2.getTrackbarPos("max_radius", window_name)
 
-        src = generate_image(filename, min_centre_distance, edge_threshold, centre_threshold, min_radius, max_radius)
+        src = generate_image(filename, min_dist, edge_threshold, centre_threshold, min_radius, max_radius)
 
         cv2.imshow(window_name, src)
         key = cv2.waitKey(1) & 0xFF
@@ -43,11 +43,11 @@ def main(argv):
     return 0
 
 
-def generate_image(filename, min_centre_distance, edge_threshold, centre_threshold, min_radius, max_radius):
+def generate_image(filename, min_dist, edge_threshold, centre_threshold, min_radius, max_radius):
     src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, min_centre_distance,
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, min_dist,
                               param1=edge_threshold, param2=centre_threshold,
                               minRadius=min_radius, maxRadius=max_radius)
     if circles is not None:
